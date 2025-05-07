f2e28990fc1b47b9270e959aeb4b936c7e21ca57
SamraAzizi <samarraazizi@gmail.com>
2025-05-07 10:45:15 +0500

    langGraph

--- main.py
+++ main.py
@@ -8,3 +8,18 @@ from typing_extensions import TypedDict
 
 
 load_dotenv()
+
+llm = init_chat_model(
+    "anthropic:claude-3-5-sonnet-latest"
+)
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+graph_builder = StateGraph(State)
+
+def chatbot(state:State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge(END, "chatbot")
