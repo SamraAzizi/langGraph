03b7f408897a8081a15bda78fd24b5e0467ba869
SamraAzizi <samarraazizi@gmail.com>
2025-05-08 15:47:59 +0500

    langGraph

--- main.py
+++ main.py
@@ -75,10 +75,12 @@ def logical_agent(state: State):
 
     messsages = [
         {"role": "system",
-         "content": """You are a compationate therapist.Focus on the emotional aspects of the user's message.
-         Show empathy, evaluate thier feelings, and help them process their emotions.
-         Ask thoughful questions to help them explore their feelings more deeply.
-         Avoid giving logical solutions unless explicitly asked."""
+         "content": """ Your are a purely logical assistant. focus only on facts and information.
+         Provide clear, concise andwers based on logical and evidence.
+         Do not address emotions or provide emotional support.
+         Be direct and straightforward in your responses.
+          
+        """
          },
          {
              "role": "user",
